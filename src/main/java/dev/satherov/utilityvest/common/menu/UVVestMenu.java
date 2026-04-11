bd007b9a7733b507abb57b52cf123da0c74221c5
Satherov <satherov@users.noreply.github.com>
2026-04-12 00:58:00 +0200

    how does this shit still exist

--- src/main/java/dev/satherov/utilityvest/common/menu/UVVestMenu.java
+++ src/main/java/dev/satherov/utilityvest/common/menu/UVVestMenu.java
@@ -79,6 +79,20 @@ public abstract class UVVestMenu extends AbstractContainerMenu {
         return player.isAlive();
     }
     
+    @Override
+    public void clicked(int slotId, int button, ClickType clickType, Player player) {
+        if (slotId >= 0 && slotId < (this.rows * 9)) {
+            final Slot slot = this.slots.get(slotId);
+            ClickAction action = button == 0 ? ClickAction.PRIMARY : ClickAction.SECONDARY;
+            final ItemStack stack = slot.getItem();
+            final ItemStack carried = this.getCarried();
+            
+            if (stack.overrideStackedOnOther(slot, action, player) || stack.overrideOtherStackedOnMe(carried, slot, action, player, this.createAccess())) return;
+        }
+        
+        super.clicked(slotId, button, clickType, player);
+    }
+    
     private SlotAccess createAccess() {
         return new SlotAccess() {
             @Override
